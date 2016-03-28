a3acebc1bfe1bb3e7e170246840dfbdb96b03e15
lingtran <lxiutran@gmail.com>
2016-03-27 20:35:52 -0600

    updated 001-create-robots migration file

--- db/migrate/001_create_robots.rb
+++ db/migrate/001_create_robots.rb
@@ -13,14 +13,3 @@ require 'sequel'
       String :department
     end
 end
-
-database.create_table :robots do
-  primary_key :id
-  String :avatar
-  String :name
-  String :city
-  String :state
-  String :birthdate
-  String :date_hired
-  String :department
-end
